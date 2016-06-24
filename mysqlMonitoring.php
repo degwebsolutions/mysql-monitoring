7406a448033ee731ef1a9279d675dd8090f27eb5
Povilas Brilius <pbrilius@gmail.com>
2016-06-24 19:28:04 +0300

    Concatenation adjusted to non-tight

--- mysqlMonitoring.php
+++ mysqlMonitoring.php
@@ -9,7 +9,7 @@ $config = [
     'delayInterval'  => 60,
 ];
 $hostName = system('hostname');
-$dbh      = new PDO('mysql:host='.$config['host'].'', $config['mysqlUser'], $config['mysqlPassword']);
+$dbh      = new PDO('mysql:host=' . $config['host'] . '', $config['mysqlUser'], $config['mysqlPassword']);
 $sth      = $dbh->prepare('SHOW STATUS');
 $timezone = date_default_timezone_get();
 
@@ -26,13 +26,13 @@ while (1) {
         } elseif ($row['Value'] == 'ON') {
             $row['Value'] = 't';
         } elseif (!preg_match('/^\d\.?\d*$/', $row['Value'])) {
-            $row['Value'] = '\"'.$row['Value'].'\"';
+            $row['Value'] = '\"' . $row['Value'] . '\"';
         }
 
-        $cmd = 'curl -i -u '.$config['influxUser'].':'.$config['influxPassword'].' -XPOST http://localhost:8086/write?db='.
-            $config['influxDb'].' --data-binary "'.$row['Variable_name'].',host='.$hostName.',region='.$timezone.
-            ' value='.(empty($row['Value']) ? '0i' : is_int($row['Value']) ? $row['Value'].'i' : $row['Value']).' '.$nanotime.'"';
-        echo shell_exec($cmd)."\n";
+        $cmd = 'curl -i -u ' . $config['influxUser'] . ':' . $config['influxPassword'] . ' -XPOST http://localhost:8086/write?db=' .
+            $config['influxDb'] . ' --data-binary "' . $row['Variable_name'] . ',host=' . $hostName . ',region=' . $timezone .
+            ' value=' . (empty($row['Value']) ? '0i' : is_int($row['Value']) ? $row['Value'] . 'i' : $row['Value']) . ' ' . $nanotime . '"';
+        echo shell_exec($cmd) . "\n";
     }
 
     sleep($config['delayInterval']);
