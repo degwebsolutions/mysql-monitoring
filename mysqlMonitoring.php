8890d2754df565926f9cb6545ef00748220e3f71
Povilas Brilius <pbrilius@gmail.com>
2016-07-02 00:43:10 +0300

    Rewritten with InfluxDB SDK

--- mysqlMonitoring.php
+++ mysqlMonitoring.php
@@ -1,38 +1,56 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
+
 $config = [
     'mysqlUser'      => 'root',
     'mysqlPassword'  => 'root',
-    'host'           => '127.0.0.1',
+    'mysqlHost'      => '127.0.0.1',
+    'influxHost'     => 'localhost',
+    'influxPort'     => 8086,
     'influxUser'     => 'mysqlMetrics',
     'influxPassword' => 'mysqlMetrics',
     'influxDb'       => 'mysqlMetrics',
     'delayInterval'  => 60,
 ];
 $hostName = system('hostname');
-$dbh      = new PDO('mysql:host=' . $config['host'] . '', $config['mysqlUser'], $config['mysqlPassword']);
+$dbh      = new PDO('mysql:host=' . $config['mysqlHost'] . '', $config['mysqlUser'], $config['mysqlPassword']);
 $sth      = $dbh->prepare('SHOW STATUS');
 $timezone = date_default_timezone_get();
 
-while (1) {
-    $nanotime = system('date +%s%N');
-    $sth->execute();
-    $res = $sth->fetchAll();
+$database = InfluxDB\Client::fromDSN(sprintf('influxdb://' . $config['influxUser'] . ':' . $config['influxPassword'] . '@%s:%s/%s', $config['influxHost'], $config['influxPort'], $config['influxDb']));
 
+$client = $database->getClient();
+
+while (1) {
+    $timeStamp = system('date +%s%N');
+    $sth->execute();
+    $res    = $sth->fetchAll();
+    $points = [];
+    echo json_encode($res) . "\n";
     foreach ($res as $row) {
         $row['Value'] = trim($row['Value']);
 
         if ($row['Value'] == 'OFF') {
-            $row['Value'] = 'f';
+            $row['Value'] = false;
         } elseif ($row['Value'] == 'ON') {
-            $row['Value'] = 't';
-        } elseif (!preg_match('/^\d\.?\d*$/', $row['Value'])) {
-            $row['Value'] = '\"' . $row['Value'] . '\"';
+            $row['Value'] = true;
+        } elseif (preg_match('/^\d\.\d*$/', $row['Value'])) {
+            $row['Value'] = (float) $row['Value'];
+        } elseif (preg_match('/^\d+$/', $row['Value'])) {
+            $row['Value'] = (int) $row['Value'];
         }
 
-        $cmd = 'curl -i -u ' . $config['influxUser'] . ':' . $config['influxPassword'] . ' -XPOST http://localhost:8086/write?db=' .
-            $config['influxDb'] . ' --data-binary "' . $row['Variable_name'] . ',host=' . $hostName . ',region=' . $timezone .
-            ' value=' . (empty($row['Value']) ? '0i' : is_int($row['Value']) ? $row['Value'] . 'i' : $row['Value']) . ' ' . $nanotime . '"';
-        echo shell_exec($cmd) . "\n";
+        $points[] = new InfluxDB\Point(
+            $row['Variable_name'], // name of the measurement
+            $row['Value'],         // the measurement value
+            [
+                'host'   => $hostName,
+                'region' => $timezone,
+            ],         // optional tags
+            [],        // optional additional fields
+            $timeStamp // Time precision has to be set to seconds!
+        );
+        $database->writePoints($points);
     }
 
     sleep($config['delayInterval']);
